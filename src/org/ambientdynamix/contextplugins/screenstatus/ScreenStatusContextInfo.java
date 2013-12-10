0690f2df7514f74e957e4e0536505010a748cacf
Lukas Ruge <ruge@itm.uni-luebeck.de>
2013-12-10 13:16:27 +0100

    now supports rdf

--- src/org/ambientdynamix/contextplugins/screenstatus/ScreenStatusContextInfo.java
+++ src/org/ambientdynamix/contextplugins/screenstatus/ScreenStatusContextInfo.java
@@ -117,9 +117,21 @@ public class ScreenStatusContextInfo implements IContextInfo, IDeviceScreenConte
 			}
 			return "<screenstatus>"+result+"</screenstatus>";
 		}
+		else if(format.equalsIgnoreCase("RDF/XML"))
+		{
+			result="<rdf:RDF\n" +
+					"xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
+					"xmlns:z.0=\"http://dynamix.org/semmodel/org.ambientdynamix.contextplugins.context.info.device.screen/0.1/\"\n" +
+					"xmlns:z.1=\"http://dynamix.org/semmodel/0.1/\" > \n";
+			result=result+" <rdf:Description rdf:about=\"http://purl.org/device_RANDOMID\">\n";
+			result=result+" <rdf:type>http://purl.org/device/screenstatus</rdf:type>\n";
+			result=result+"<z.0:status>"+screenstatus+"</z.0:status>\n" +
+							"  </rdf:Description>\n </rdf:RDF>";
+			return result;
+		}
 		else if (format.equalsIgnoreCase("JSON"))
 		{
-			return " ";
+			return " { \"screen\": \""+screenstatus+"\"}";
 		}
 		else
 			return null;
@@ -132,6 +144,7 @@ public class ScreenStatusContextInfo implements IContextInfo, IDeviceScreenConte
 		formats.add("text/plain");
 		formats.add("XML");
 		formats.add("JSON");
+		formats.add("RDF/XML");
 		return formats;
 	}
 
